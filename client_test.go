685ca80c46aa86c4d262ac2ef67a94967f46b1dd
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-26 09:36:55 +0300

    test client done

--- client_test.go
+++ client_test.go
@@ -1,1 +1,16 @@
 package rin
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestProcessRequest(t *testing.T) {
+	client := NewClient()
+	router := NewRouter()
+	router.RegisterFunc(200, func(resp *http.Response, content interface{}) error {
+		return nil;
+	})
+	res := NewRessource("/get", "GET", router)
+	if err := client.ProcessRequest("http://httpbin.org", res, nil); err != nil{ t.Fail()}
+}
