0e337b8c88f856aad08725c3dfd45a75a2f1e5e8
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-26 10:10:29 +0300

    test done

--- api_test.go
+++ api_test.go
@@ -22,3 +22,22 @@ func TestAPICall(t *testing.T){
 	}
 
 }
+func TestAPIAuth(t *testing.T) {
+	api := NewApi("https://httpbin.org")
+	router := NewRouter()
+	router.RegisterFunc(200, func(resp *http.Response, _ interface{}) error {
+		return nil
+	})
+	res := NewRessource("/basic-auth/{{.user}}/{{.pass}}", "GET", router)
+	api.AddRessource("basicauth", res)
+	api.SetAuth(&AuthBasic{
+		Username: "user",
+		Password: "passw0rd",
+	})
+	if err := api.Call("basicauth", map[string]string{
+		"user": "user",
+		"pass": "passw0rd",
+	}); err != nil {
+		t.Fail()
+	}
+}
--- ressource_test.go
+++ ressource_test.go
@@ -1,1 +1,17 @@
 package rin
+
+import "testing"
+
+func TestEndpointTemplate(t *testing.T) {
+	res := &RestRessources{
+		Endpoint: "/user/{{.user}}",
+		Method:   "GET",
+		Router:   NewRouter(),
+	}
+	renderedEndpoint := res.RenderEndpoint(map[string]string{
+		"user": "matt",
+	})
+	if renderedEndpoint != "/user/matt" {
+		t.Fail()
+	}
+}
