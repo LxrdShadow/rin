deae6ff44d9a6e76ca7ba9a174c14a5c708a8e6f
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-26 09:44:15 +0300

    testing client done

--- api_test.go
+++ api_test.go
@@ -1,1 +1,20 @@
 package rin
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAPICall(t *testing.T){
+	api := NewApi("http://httpbin.org")
+	router:=NewRouter()
+	router.RegisterFunc(200, func(resp *http.Response,  _ interface{}) error {
+		return nil
+	})
+	res := NewRessource("/get", "GET", router)
+	api.AddRessource("get", res)
+	if err := api.Call("get", nil); err!=nil{
+		t.Fail()
+	}
+
+}
